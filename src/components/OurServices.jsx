c34bf89f17114a72d77a50d9aa2824353e08fc15
Tushar Sharma <tushar@Tushars-MacBook-Air.local>
2026-01-12 02:57:02 +0530

    whatsappssshhgghgs

--- src/components/OurServices.jsx
+++ src/components/OurServices.jsx
@@ -33,6 +33,126 @@ const services = [
     title: "PRP & Advanced Hair Procedures",
     img: "/HairPRP.jpeg",
   },
+  {
+    title: "Hair Density Treatment",
+    img: "/service1.jpeg",
+  },
+  {
+    title: "Hair Loss Treatment",
+    img: "/service2.jpeg",
+  },
+  {
+    title: "Hair Thickness Treatment",
+    img: "/service3.jpeg",
+  },
+  {
+    title: "Acne, Pimple, Zap, Acne Scars Treatment",
+    img: "/service4.jpeg",
+  },
+  {
+    title: "Chemical Peeling",
+    img: "/service5.jpeg",
+  },
+  {
+    title: "Carbon Laser Facial",
+    img: "/service6.jpeg",
+  },
+  {
+    title: "Face Meso",
+    img: "/service7.jpeg",
+  },
+  {
+    title: "Face MN Glow Treatment",
+    img: "/service8.jpeg",
+  },
+  {
+    title: "Hydrafacial (with Jelly Mask & Sheet Mask)",
+    img: "/service9.jpeg",
+  },
+  {
+    title: "IV Glow Drip Treatment",
+    img: "/service10.jpeg",
+  },
+  {
+    title: "Korean Glass Glow Facial",
+    img: "/service11.jpeg",
+  },
+  {
+    title: "Medifacial – Skin Detan, Skin Lightening, Whitening, Brightening, Aqua gold Facial",
+    img: "/service12.jpeg",
+  },
+  {
+    title: "Laser Hair Reduction – Full body & Body Part",
+    img: "/service13.jpeg",
+  },
+  {
+    title: "Skin Rejuvenation / Full Body Polishing via laser machine",
+    img: "/service14.jpeg",
+  },
+  {
+    title: "Photo Facial",
+    img: "/service15.jpeg",
+  },
+  {
+    title: "Pigmentation, Sun damage, Freckle, Dark Spots, Melasma Treatment",
+    img: "/service16.jpeg",
+  },
+  {
+    title: "Skin Glow Facial Treatment",
+    img: "/service17.jpeg",
+  },
+  {
+    title: "BB Glow Treatment & Oxygen Facial",
+    img: "/service18.jpeg",
+  },
+  {
+    title: "Tattoo Removal Treatment",
+    img: "/service19.jpeg",
+  },
+  {
+    title: "Cupping Therapy",
+    img: "/service20.jpeg",
+  },
+  {
+    title: "Mole, Wart Removal",
+    img: "/service21.jpeg",
+  },
+  {
+    title: "PMU makeup, PMU eye brow",
+    img: "/service22.jpeg",
+  },
+  {
+    title: "Fillers",
+    img: "/service23.jpeg",
+  },
+  {
+    title: "Stretch Marks Removal Treatment",
+    img: "/service24.png",
+  },
+  {
+    title: "Thread, Face lift",
+    img: "/service25.jpeg",
+  },
+  {
+    title: "Anti Ageing treatment, Face wrinkles, sagging eyelid eyebrows",
+    img: "/service26.jpeg",
+  },
+  {
+    title: "Lip Lightening Treatment",
+    img: "/service27.jpeg",
+  },
+  {
+    title: "HIFU Treatment",
+    img: "/service28.jpeg",
+  },
+  {
+    title: "Wellness Services, Slimming, Diet, Body Contouring",
+    img: "/service29.jpeg",
+  },
+  {
+    title: "Under Eye Dark Circles Treatment",
+    img: "/service30.jpeg",
+  },
 ];
 
 const OurServices = () => {
